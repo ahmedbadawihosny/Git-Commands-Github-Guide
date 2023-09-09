3ea0434694ee071d97765f0412cdb943150bd5ff
Ahmed Badawi <119693125+ahmedbadawihosny@users.noreply.github.com>
2023-09-10 00:49:50 +0300

    Delete test3 directory

--- test3/A-Sheet #1-Data type/A-Say Hello With C++.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(){
-  char name[1024] = "";
-  scanf("%s", &name);
-  printf("Hello, %s" , name);
-
-  return 0;
-}
--- test3/A-Sheet #1-Data type/E-Area of a Circle.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(){
-  float n,area;
-  scanf("%f",&n);
-  area = (n*n)*3.141592653;
-  printf("%.9f",area);
-  return 0;
-}
